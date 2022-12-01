e5d5924fbbd86a83d6c0403f8bd13fb95c1041d5
Devin Lin <dxl702@case.edu>
2022-12-01 03:27:53 -0500

    added new uri

--- pages/index.js
+++ pages/index.js
@@ -31,7 +31,7 @@ let oldTitle;
 let oldDate;
 
 export async function getServerSideProps(context) {
-  let res = await fetch("/api/posts", {
+  let res = await fetch("https://calendar-diary.vercel.app/api/posts", {
     method: "GET",
     headers: {
       "Content-Type": "application/json",
